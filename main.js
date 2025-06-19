6b78bf9386a8e56fff928a3b011bf5df97e0d797
Taimoor Ali <taimoorali202@gmail.com>
2025-06-19 21:22:59 +0500

    add calculator functionality and show output

--- main.js
+++ main.js
@@ -3,23 +3,29 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-let operand1;
-let operand2;
-let operator;
+let operand1 = '';
+let operand2 = '';
+let operator = '';
+
+let result = 0;
 
 const operate = (num1, num2, operator) => {
   switch (operator) {
     case '+':
-      add(num1, num2)
+      result = add(num1, num2);
       break;
     case '-':
-      subtract(num1, num2);
+      result = subtract(num1, num2);
       break;
     case '*':
-      multiply(num1, num2);
+      result = multiply(num1, num2);
       break;
     case '/':
-      divide(num1, num2);
+      if (num2 === 0) {
+        alert('You cannot divide with zero!');
+        return;
+      }
+      result = divide(num1, num2);
       break;
   }
 }
@@ -31,21 +37,79 @@ contentWrapper.addEventListener('click', e => {
   if (e.target.nodeName !== 'BUTTON') return;
 
   const value = e.target.value;
+  const operators = new Set(['+', '-', '/', '*']);
+  let chars = output.textContent.split('');
 
   if (value === 'AC') {
     output.textContent = '';
+    operand1 = '';
+    operand2 = '';
+    operator = '';
+    result = 0;
   } else if (value === 'DEL') {
-    if (output.lastChild) output.removeChild(output.lastChild);
+    const char = chars.pop();
+    if (operators.has(char)) {
+      operator = '';
+    } else if (chars.some(char => operators.has(char))) {
+      const operand2Arr = operand2.split('');
+      operand2Arr.pop();
+      operand2 = operand2Arr.join('');
+    } else {
+      const operand1Arr = operand1.split('');
+      operand1Arr.pop();
+      operand1 = operand1Arr.join('');
+    }
+
+    output.textContent = chars.join('');
+  } else if (value === '=') {
+    if (!operand1 || !operand2 || !operator) return;
+    prepareOperation();
   } else {
-    const operators = ['+', '-', '/', '*'];
+    let chars = output.textContent.split('');
+    if ((operand1 && operand2 && operator) && operators.has(value)) {
+      prepareOperation();
+      operator = value;
+      output.textContent += value; 
+      return;
+    }
 
-    if (
-      output.lastChild &&
-      operators.includes(output.lastChild.textContent) &&
-      operators.includes(value)
-    ) return;
+    // we don't want an operator showing when there are no operands.
+    if (!operand1 && operators.has(value)) return;
 
-    const content = document.createTextNode(value);
-    output.appendChild(content);
+    if (operator && operators.has(value)) {
+      chars.pop();
+      chars.push(value);
+      operator = value;
+      output.textContent = chars.join('');
+      return;
+    }
+
+     
+
+    if (operators.has(value)) {
+      operator = value;
+    } else if (!operator) {
+      operand1 += value;
+    } else {
+      operand2 += value;
+    }
+
+    if (chars.length === 1 && chars[0] === '0') {
+      output.textContent = value;
+      return;
+    }
+
+    output.textContent += value;
   }
+
+  
 });
+
+const prepareOperation = () => {
+  operate(parseInt(operand1), parseInt(operand2), operator);
+  const resultString = String(result);
+  operand1 = resultString;
+  operand2 = '';
+  operator = ''
+  output.textContent = resultString;
+}
